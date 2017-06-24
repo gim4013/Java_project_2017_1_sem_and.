80e704a5ed5bbde3cbfcabcbc10a0f996f36a6af
gim4013 <gim4013@naver.com>
2017-06-24 21:04:03 +0900

    LinearLayout //Indent를 이용해서 Activity 전환

--- app/src/main/java/org/androidtown/myapplication/MainActivity.java
+++ app/src/main/java/org/androidtown/myapplication/MainActivity.java
@@ -1,16 +1,31 @@
 package org.androidtown.myapplication;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
-    public void onCreate()
-    {}
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
+            //Button schedule_button = (Button)findViewById(R.id.button_schedule);
+        }
+        public void onClicked_schedule(View v)
+        {
+            Toast.makeText(getApplicationContext(),"Schedule창으로 이동합니다.",Toast.LENGTH_LONG).show();
+            Intent myIntent = new Intent(MainActivity.this,schedule.class);
+            startActivity(myIntent);
+        }
+        public void onClicked_off(View v)
+        {
+            Toast.makeText(getApplicationContext(),"Exit",Toast.LENGTH_LONG).show();
+            finish();
+        }
+
+
 
 }
---
+++ app/src/main/java/org/androidtown/myapplication/schedule.java
@@ -0,0 +1,22 @@
+package org.androidtown.myapplication;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Toast;
+
+public class schedule extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_schedule);
+    }
+    public void onClicked_add(View v)
+    {
+        Toast.makeText(getApplicationContext(),"Add the Schedule",Toast.LENGTH_LONG).show();
+        Intent myIntent = new Intent(schedule.this,schedule_add.class);
+        startActivity(myIntent);
+    }
+}
